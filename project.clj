0878da81c145ae8cf112b7f478bb2bed720639cb
Elizabeth Engelman <emengelman@gmail.com>
2017-03-25 10:16:12 -0500

    Adds Datomic to project
    Includes Datomic credentials

--- project.clj
+++ project.clj
@@ -8,7 +8,9 @@
                  [hiccup "1.0.2"]
                  [org.clojure/java.jdbc "0.2.3"]
                  [com.h2database/h2 "1.3.170"]
-                 [com.datomic/datomic-free "0.9.5561"]]
+                 [com.datomic/datomic-pro "0.9.5561"]]
+  :repositories  {"my.datomic.com" {:url "https://my.datomic.com/repo"
+                                    :creds :gpg}}
   :plugins [[lein-ring "0.9.7"]]
   :ring {:handler clojure-west-app.handler/app}
   :profiles
