01e4b646ac46bf9306b2ef2b0e455c9e07a38b17
Elizabeth Engelman <emengelman@gmail.com>
2017-03-25 18:18:07 -0500

    Refactors user->animals
    breaking it into 2 smaller queries

--- src/clojure_west_app/paws_data.clj
+++ src/clojure_west_app/paws_data.clj
@@ -4,19 +4,25 @@
 (def users-and-groups
   [{:db/id (d/tempid :db.part/user -1)
     :user/type "public"}
+   {:db/id (d/tempid :db.part/user -2)
+    :user/type "employee"}
+
    {:db/id (d/tempid :db.part/user -3)
     :animal/type "cat"
     :animal/name "Gene"}
+
+   {:db/id (d/tempid :db.part/user -4)
+    :animal/type "cat"
+    :animal/name "Louise"}
+
+   {:db/id (d/tempid :db.part/user -5)
+    :animal/type "fish"
+    :animal/name "Lil' Blue"}
+
    {:group/name "Public Visibility Group"
     :group/member [(d/tempid :db.part/user -1)
                    (d/tempid :db.part/user -3)]}
 
-
-   {:db/id (d/tempid :db.part/user -2)
-    :user/type "employee"}
-   {:db/id (d/tempid :db.part/user -4)
-    :animal/type "cat"
-    :animal/name "Louise"}
    {:group/name "Employee Visibility Group"
     :group/member [(d/tempid :db.part/user -2)
                    (d/tempid :db.part/user -4)]}])
--- src/clojure_west_app/query.clj
+++ src/clojure_west_app/query.clj
@@ -7,17 +7,41 @@
 (defn db-value []
   (d/db (db/connection db-name)))
 
-(defn default-query [group-name]
-  `[:find ?a
-    :where
-     [?a :animal/name]
-     [?g :group/name ~group-name]
-     [?g :group/member ?a]])
+(defn user->group [user]
+  (d/q '[:find ?g
+         :in $ ?user-type
+         :where
+         [?u :user/type ?user-type]
+         [?g :group/member ?u]]
+       (db-value)
+       (:user-type user)))
+
+(defn groups->animals [groups]
+  (d/q '[:find ?a
+         :in $ [?group ...]
+         :where
+         [?group :group/member ?a]
+         [?a :animal/name]]
+       (db-value)
+       groups))
+
+(defn second-user->animals [user]
+  (let [groups (flatten (into [] (user->group user)))]
+    (groups->animals groups)))
+
+(defn first-user->animals [user]
+  (d/q '[:find ?a
+         :in $ ?user-type
+         :where [?u :user/type ?user-type]
+         [?g :group/member ?u]
+         [?g :group/member ?a]
+         [?a :animal/name]]
+       (db-value)
+       (:user-type user)))
 
 (defmulti user->animals (fn [user] (:custom-query user)))
 (defmethod user->animals :special-client [user]
   "use the special-client query")
 (defmethod user->animals :default [user]
-  (d/q (default-query (:group-name user))
+  (d/q (default-query (:user-type user))
        (db-value)))
-
