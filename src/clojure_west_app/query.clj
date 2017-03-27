6010483881a92ad1dead2de5a0e9b1f19fc68540
Elizabeth Engelman <emengelman@gmail.com>
2017-03-27 08:41:25 -0500

    WIP - adds in the particular client
    Still having issues with the new definition of the user->animal rule

--- src/clojure_west_app/query.clj
+++ src/clojure_west_app/query.clj
@@ -20,26 +20,26 @@
          db-value
          (:user-type user))))
 ;============================================
-;Iteration 2: breaking it into 2 composable queries
+;Iteration 2: breaking it into 2 separate queries
 ;============================================
-(defn user->group [user db-value]
-  (d/q '[:find ?g
-         :in $ ?user-type
-         :where
-         [?u :user/type ?user-type]
-         [?g :group/member ?u]]
-       db-value
-       (:user-type user)))
-
-(defn groups->animals [groups db-value]
-  (d/q '[:find ?a
-         :in $ [?group ...]
-         :where
-         [?group :group/member ?a]
-         [?a :animal/name]]
-       db-value
-       groups))
-
+;(defn user->group [user db-value]
+;  (d/q '[:find ?g
+;         :in $ ?user-type
+;         :where
+;         [?u :user/type ?user-type]
+;         [?g :group/member ?u]]
+;       db-value
+;       (:user-type user)))
+;
+;(defn groups->animals [groups db-value]
+;  (d/q '[:find ?a
+;         :in $ [?group ...]
+;         :where
+;         [?group :group/member ?a]
+;         [?a :animal/name]]
+;       db-value
+;       groups))
+;
 ;(defn second-user->animals [user db-value]
 ;  (let [groups (flatten (into [] (user->group user db-value)))]
 ;    (groups->animals groups db-value)))
@@ -57,11 +57,6 @@
     [?g :group/member ?a]
     [?a :animal/name]]])
 
-(def user->animal ;not using this rulset in iteration 2b
-  '[[(user->animal ?user-type ?g ?a)
-     (user-group ?user-type ?g)
-     (group-animal ?g ?a)]])
-
 (def rules
   (concat user-type->group-rule
           group->animal-rule))
@@ -70,7 +65,6 @@
   (d/q '[:find ?a
          :in $ % ?user-type
          :where
-         [?a :animal/type]
          (group->animal ?g ?a)
          (user-type->group ?user-type ?g)]
        db-value
@@ -81,29 +75,27 @@
 ;Iteration 3: adding another data shape (special client)
 ;============================================
 
-;(def user->animal
-;  '[[(user->animal)
-;     [?a :animal/type]
-;     (user-group ?user-type)
-;     (group-animal ?g)]])
+(def special-user->animal-rule
+  '[[(user->animal ?user-type ?g ?a ?user-email)
+     [?u :user/email ?user-email]
+     [?u :user/assigned-animal ?a]]])
 
-  ;[[(special-user->animal ?user-email ?user-type)
-  ;  [?u :user/email ?user-email]
-  ;  [?u :user/assigned-animal ?a]]
-  ; [(special-user->animal ?user-email ?user-type)
-  ;  [?u :user/type ?user-type]
-  ;  [?g :group/member ?u]
-  ;  [?g :group/member ?a]
-  ;  [?a :animal/name]]]
+(def default-user->animal-rule
+  '[[(user->animal ?user-type ?g ?a ?user-email)
+    [?u :user/type ?user-type]
+    [?g :group/member ?u]
+    [?g :group/member ?a]
+    [?a :animal/name]]])
+
 (defn special-user->animals [user db-value]
-  (if (:user-email user)
-    (d/q '[:find ?a
-           :in $ ?user-email
-           :where
-           [?u :user/email ?user-email]
-           [?u :user/assigned-animal ?a]]
-         db-value
-         (:user-email user))))
+  (d/q '[:find ?a
+         :in $ % ?user-email ?user-type
+         :where
+         (user->animal ?user-type ?g ?a ?user-email)]
+       db-value
+       (concat default-user->animal-rule special-user->animal-rule)
+       (:user-email user "no-email")
+       (:user-type user)))
 
 (defmulti third-user->animals (fn [user db-value] (:custom-query user)))
 (defmethod third-user->animals :special-client [user db-value]
